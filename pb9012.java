0cb4ecc0496ddb9870b09f2e6bf80723a0a96b39
Sim Kyu Young <skq789@gmail.com>
2018-07-29 20:49:38 +0900

    18.07.29 / 987B / 84ms, 10268KB, Correct

---
+++ pb9012.java
@@ -0,0 +1,48 @@
+package algo_baek;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+
+
+public class pb9012 {
+	public static void main (String args[]) throws IOException {
+		
+		BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(BR.readLine());
+		
+		String s;
+			
+		for(int i = 0; i < T; i++) {
+			s = BR.readLine();
+			Stack st = new Stack();
+			
+			for(int j = 0; j< s.length(); j++) {
+				if( s.charAt(j) == '(') {
+					st.push("(");
+				} else if( s.charAt(j) == ')') {
+					if(st.isEmpty()) {
+						st.push("x");
+						break;
+					} else {
+						st.pop();
+					}
+				}
+			}
+			
+			if(st.isEmpty()) {
+				BW.write("YES\n");
+			} else {
+				BW.write("NO\n");
+			}
+		}
+		BR.close();
+		BW.flush();
+		BW.close();
+	}
+}
